7cb2cf6fccafd188b8f4ea8414505d58c12409a7
Zhening <18501262663@163.com>
2021-11-14 01:38:37 +0800

    feat: complete page collector for subarea

--- main.go
+++ main.go
@@ -1,84 +1,115 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"strconv"
 
 	"github.com/gocolly/colly"
-	"github.com/gocolly/colly/queue"
 )
 
 func main() {
 
-	urlPrefix := "https://bj.lianjia.com"
+	// urlPrefix := "https://bj.lianjia.com"
 
-	areaCount := 0
-	// Instantiate area collector
-	areaCollector := colly.NewCollector(
-		// Visit only domains: lianjia.com, bj.lianjia.com
-		colly.AllowedDomains("lianjia.com", "bj.lianjia.com"),
-		//colly.Debugger(&debug.LogDebugger{}),
-	)
+	// areaCount := 0
+	// // Instantiate area collector
+	// areaCollector := colly.NewCollector(
+	// 	// Visit only domains: lianjia.com, bj.lianjia.com
+	// 	colly.AllowedDomains("lianjia.com", "bj.lianjia.com"),
+	// 	//colly.Debugger(&debug.LogDebugger{}),
+	// )
 
-	// areaQueue is a rate limited queue
-	areaQueue, _ := queue.New(
-		1,                                        // Number of consumer threads
-		&queue.InMemoryQueueStorage{MaxSize: 20}, // Use default queue storage
-	)
+	// // areaQueue is a rate limited queue
+	// areaQueue, _ := queue.New(
+	// 	1,                                        // Number of consumer threads
+	// 	&queue.InMemoryQueueStorage{MaxSize: 20}, // Use default queue storage
+	// )
 
-	// Before making a request print "Visiting ..."
-	areaCollector.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting", r.URL.String())
-	})
+	// // Before making a request print "Visiting ..."
+	// areaCollector.OnRequest(func(r *colly.Request) {
+	// 	log.Println("Visiting", r.URL.String())
+	// })
 
-	// On every an element which has <div data-role="ershoufang"/> attribute call callback
-	areaCollector.OnHTML("div[data-role='ershoufang']", func(e *colly.HTMLElement) {
-		e.ForEach("a", func(_ int, e *colly.HTMLElement) {
-			urlSuffix := e.Attr("href")
-			link := urlPrefix + urlSuffix
-			areaQueue.AddURL(link)
-			log.Printf("Adding Area URL [%d]: %s", areaCount, link)
-		})
-	})
+	// // On every an element which has <div data-role="ershoufang"/> attribute call callback
+	// areaCollector.OnHTML("div[data-role='ershoufang']", func(e *colly.HTMLElement) {
+	// 	e.ForEach("a", func(_ int, e *colly.HTMLElement) {
+	// 		urlSuffix := e.Attr("href")
+	// 		link := urlPrefix + urlSuffix
+	// 		areaQueue.AddURL(link)
+	// 		log.Printf("Adding Area URL [%d]: %s", areaCount, link)
+	// 	})
+	// })
 
-	// subAreaStore can check whether a URL has already been visited
-	subAreaStore := make(map[string]bool)
-	subAreaCount := 0 // Count of subArea URLs
+	// // subAreaStore can check whether a URL has already been visited
+	// subAreaStore := make(map[string]bool)
+	// subAreaCount := 0 // Count of subArea URLs
 
-	// Instantiate subArea collector
-	subAreaCollector := colly.NewCollector(
+	// // Instantiate subArea collector
+	// subAreaCollector := colly.NewCollector(
+	// 	// Visit only domains: lianjia.com, bj.lianjia.com
+	// 	colly.AllowedDomains("lianjia.com", "bj.lianjia.com"),
+	// 	// colly.Debugger(&debug.LogDebugger{}),
+	// )
+
+	// subAreaQueue, _ := queue.New(
+	// 	3, // Number of consumer threads
+	// 	&queue.InMemoryQueueStorage{MaxSize: 300}, // Use default queue storage
+	// )
+
+	// // Before making a request print "Visiting ..."
+	// subAreaCollector.OnRequest(func(r *colly.Request) {
+	// 	log.Println("Visiting", r.URL.String())
+	// })
+
+	// subAreaCollector.OnHTML("div[data-role=ershoufang] > div:nth-child(2)", func(e *colly.HTMLElement) {
+	// 	e.ForEach("a", func(_ int, e *colly.HTMLElement) {
+
+	// 		urlSuffix := e.Attr("href")
+	// 		// check whether the url has been visited
+	// 		if !subAreaStore[urlSuffix] {
+	// 			// add the url to the store
+	// 			subAreaStore[urlSuffix] = true
+	// 			subAreaCount += 1
+	// 			link := urlPrefix + urlSuffix
+	// 			// add the subarea url to the queue
+	// 			subAreaQueue.AddURL(link)
+	// 			log.Printf("Adding SubArea URL [%d]: %s", subAreaCount, link)
+	// 		}
+	// 	})
+	// })
+
+	// // Start scraping ershoufang information
+	// areaCollector.Visit(urlPrefix + "/ershoufang/")
+	// areaQueue.Run(subAreaCollector)
+
+	type pageData struct {
+		TotalPage int `json:"totalPage"`
+		CurPage   int `json:"curPage"`
+	}
+
+	// Instantiate page collector
+	pageCollector := colly.NewCollector(
 		// Visit only domains: lianjia.com, bj.lianjia.com
 		colly.AllowedDomains("lianjia.com", "bj.lianjia.com"),
 		// colly.Debugger(&debug.LogDebugger{}),
 	)
 
-	subAreaQueue, _ := queue.New(
-		3, // Number of consumer threads
-		&queue.InMemoryQueueStorage{MaxSize: 300}, // Use default queue storage
-	)
-
-	// Before making a request print "Visiting ..."
-	subAreaCollector.OnRequest(func(r *colly.Request) {
+	pageCollector.OnRequest(func(r *colly.Request) {
 		log.Println("Visiting", r.URL.String())
 	})
 
-	subAreaCollector.OnHTML("div[data-role=ershoufang] > div:nth-child(2)", func(e *colly.HTMLElement) {
-		e.ForEach("a", func(_ int, e *colly.HTMLElement) {
+	pageCollector.OnHTML("div.page-box.house-lst-page-box", func(e *colly.HTMLElement) {
 
-			urlSuffix := e.Attr("href")
-			// check whether the url has been visited
-			if !subAreaStore[urlSuffix] {
-				// add the url to the store
-				subAreaStore[urlSuffix] = true
-				subAreaCount += 1
-				link := urlPrefix + urlSuffix
-				// add the subarea url to the queue
-				subAreaQueue.AddURL(link)
-				log.Printf("Adding SubArea URL [%d]: %s", subAreaCount, link)
-			}
-		})
+		var page pageData
+		json.Unmarshal([]byte(e.Attr("page-data")), &page)
+		log.Printf("Adding %d pages for %s", page.TotalPage, e.Request.URL.String())
+		for i := 1; i <= page.TotalPage; i++ {
+			link := e.Request.URL.String() + "pg" + strconv.Itoa(i) + "/"
+			log.Printf("Adding Page URL [%d]: %s", i, link)
+		}
 	})
 
-	// Start scraping ershoufang information on https://lianjia.com/
-	areaCollector.Visit(urlPrefix + "/ershoufang/")
-	areaQueue.Run(subAreaCollector)
+	pageCollector.Visit("https://bj.lianjia.com/ershoufang/andingmen/")
+
 }
